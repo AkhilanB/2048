338bb6f39155487218610da523674b7b723f5e2d
AkhilanB <akhilan.boopathy@lakesideschool.org>
2014-06-05 10:26:05 -0700

    Update game_manager.js

--- js/game_manager.js
+++ js/game_manager.js
@@ -180,11 +180,6 @@ GameManager.prototype.move = function (direction) {
           // Update the score
           self.score += merged.value;
 
-          // The mighty 2048 tile
-          if (merged.value === 2048) self.won = true;
-        } else {
-          self.moveTile(tile, positions.farthest);
-        }
 
         if (!self.positionsEqual(cell, tile)) {
           moved = true; // The tile moved from its original cell!
