2c98d98aafdca7d537c23c477b115a0c45ee1bc9
AkhilanB <akhilan.boopathy@lakesideschool.org>
2014-06-07 13:19:49 -0700

    Update ai.js

--- js/ai.js
+++ js/ai.js
@@ -1,4 +1,6 @@
 function Ai() {
+    this.maxIter = 2;
+
     this.init = function() {
         // This method is called when AI is first initialized.
     }
@@ -6,63 +8,90 @@ function Ai() {
     this.restart = function() {
         // This method is called when the game is reset.
     }
-	
-	var MAX_SEARCH_DEPTH = 6;
-	
-	function gridSum(grid) {
-		var sum = 0;
-		for (var i = 0; i < 4; i++) {
-			for (var j = 0; j < 4; j++) {
-				if (grid.cells[i][j] != null) {
-					sum ++;
-				}
-			}
-		}
-		return 1;
-	}
-    
-	
-	function gridSum(grid) {
-		return rateAverageValue(grid);
-	}
-	
-	function dfs(grid, depth) {
-		var ret = {dir: 0, avgRating: -1000000};
-		if (depth >= MAX_SEARCH_DEPTH) {
-			ret.avgRating = rateGrid(grid);
-			return ret;
-		}
-		
-		for (var dir = 0; dir < 4; dir++) {
-			var grid2 = grid.copy();
-			if (grid2.move(dir)) {
-				var moveRating = 0.00001;
-                var cells = grid2.availableCells();
-                if (cells.length == 0) continue;
-                for (var i = 0; i < cells.length; i++) {
-                    var tile = new Tile(cells[i],2);
-                    grid2.insertTile(tile);
-                    moveRating += dfs(grid2, depth+1).avgRating;
-				    grid2.removeTile(tile);
+
+    this.weight_grid = function(dir, grid, iter) {
+        var copy = grid.copy();
+        var moved = copy.move(dir);
+        if (!moved) return null;
+
+        var weights = { 
+            dir: dir,
+            piecesLost: function() { return this.originalPieceCount - this.afterMovePieceCount; },
+            totalPiecesLost: function(i) { 
+                if (this.extra_weight == null) return this.piecesLost() / i;
+                return (this.piecesLost() / i) + this.extra_weight.totalPiecesLost(i + 1);
+            },
+            totalPieceValue: function(i) { 
+                if (this.extra_weight == null) return this.pieceValue / i;
+                return (this.piecesValue / i) + this.extra_weight.totalPieceValue(i + 1);
+            },
+        };
+
+        weights.originalPieceCount = 16 - grid.availableCells().length;
+        weights.afterMovePieceCount = 16 - copy.availableCells().length;
+
+        var total = 0;
+        copy.eachCell(function(x, y, cell) { if (cell) total += Math.pow(cell.value, 2); });
+        weights.pieceValue = total;
+
+        weights.pairsNextToEachOther = 0;
+        copy.eachCell(function(x, y, cell) {
+            if (!cell) return;
+
+            for (var x2 = x + 1; x2 < 4; x2++) {
+                var cell2 = copy.cells[x2][y];
+                if (cell2 != null) {
+                    if (cell.value == cell2.value) weights.pairsNextToEachOther++;
+                    break;
                 }
-                
-                //console.log(dir, moveRating, cells.length, moveRating / cells.length)
-                
-                moveRating /= cells.length;
-                
-                //console.log(dir, moveRating);
-                
-                if (moveRating > ret.avgRating) {
-					ret.avgRating = moveRating;
-					ret.dir = dir;
-				}
-			}
-		}
-        //console.log(ret.dir);
-		return ret;
-	}
-	
+            }
+
+            for (var y2 = y + 1; y2 < 4; y2++) {
+                var cell2 = copy.cells[x][y2];
+                if (cell2 != null) {
+                    if (cell.value == cell2.value) weights.pairsNextToEachOther++;
+                    break;
+                }
+            }
+        });
+
+        var extra_weights = null;;
+        if (iter < this.maxIter) {
+            extra_weights = this.get_best(copy, iter + 1);
+        }
+        weights.extra_weight = extra_weights;
+        return weights;
+    }
+
+    this.compare_weights = function(a, b) {
+        if (a == null && b == null) return null;
+        if (b == null) return a;
+        if (a == null) return b;
+
+        var aTotal = a.totalPiecesLost(1);
+        var bTotal = b.totalPiecesLost(1);
+        if (aTotal > bTotal) return a;
+        if (aTotal < bTotal) return b;
+
+        aTotal = a.totalPieceValue(1);
+        bTotal = b.totalPieceValue(1);
+        if (aTotal > bTotal) return a;
+        if (aTotal < bTotal) return b;
+
+        return a;
+    }
+
+    this.get_best = function(grid, iter) {
+        var max = null;
+        for (var i = 0; i < 4; i++) {
+            var weights = this.weight_grid(i, grid, iter);
+            max = this.compare_weights(weights, max);
+        }
+        return max;
+    }
+
     this.step = function(grid) {
-		return dfs(grid, 0).dir;
+        var best = this.get_best(grid, 1);
+        return best.dir;
     }
 }
